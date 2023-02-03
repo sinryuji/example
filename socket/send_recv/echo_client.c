be3a2f63e010b8b1aa71fb4fc1c618b7a3d26157
sinryuji <siler2474@gmail.com>
2023-02-03 18:27:30 +0900

    add: socket/send_recv/

---
+++ socket/send_recv/echo_client.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define BUF_SIZE 1024
+void error_handling(char *message);
+
+int main(int argc, char **argv)
+{
+	int sock;
+	struct sockaddr_in serv_addr;
+	char message[BUF_SIZE];
+	int str_len;
+
+	if (argc != 3)
+	{
+		printf("Usage : %s <port>\n", argv[0]);
+		exit(1);
+	}
+
+	sock = socket(PF_INET, SOCK_STREAM, 0);									   	// 소켓을 생서앟고 있다. 소켓을 생성하는 순간에는 서버 소켓과 클라이언트 소켓이 나뉘어 지지 않는다.
+																				// bind, listen 함수의 호출이 이어지면 서버 소켓이 되는 것이고, connect 함수 호출이 이어지면 클라이언트 소켓이 되는 것이다.
+	if (sock == -1)
+		error_handling("socket() error");
+
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family=AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+	serv_addr.sin_port = htons(atoi(argv[2]));
+
+	if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)   // connect 함수 호출을 통해 서버 프로그램에 연결을 요청하고 있다.
+		error_handling("connect() error!");
+	else
+		puts("Connected..........");
+
+	while(1)
+	{
+		fputs("Input message(Q to quit): ", stdout);
+		fgets(message, BUF_SIZE, stdin);
+		
+		if (!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
+			break;
+
+		send(sock, message, strlen(message), 0);
+	}
+	close(sock);
+	return 0;
+}
+
+void error_handling(char *message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
